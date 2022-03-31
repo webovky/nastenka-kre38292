dd05cfbe394898351a5d9f285bfcceb9381b1555
Filip Krejčí <kre38292@spseool.cz>
2022-03-31 14:18:28 +0200

    31.3.

--- webface/routes.py
+++ webface/routes.py
@@ -1,6 +1,10 @@
 from . import app
-from flask import render_template, request, redirect, url_for, session
+from flask import render_template, request, redirect, url_for, session, flash
 import functools
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlite3
+dbfile="databaze.sqlite"
+conn=sqlite3.connect("databaze.sqlite", isolation_level=None)
 
 # from werkzeug.security import check_password_hash
 
@@ -23,14 +27,48 @@ def index():
     return render_template("base.html.j2")
 
 
-@app.route("/info/")
-def info():
-    return render_template("info.html.j2")
+@app.route("/login/")
+def login():
+    return render_template("login.html.j2")
 
 
-@app.route("/abc/")
-def abc():
-    return render_template("abc.html.j2", slova=slova)
+@app.route("/login/", methods=["POST"])
+def login_post():
+    nick = request.form.get("nick")
+    passwd = request.form.get("passwd")
+    if nick and passwd:
+        with sqlite3.connect(dbfile) as conn:
+            tabulka =list(conn.execute("SELECT passwd FROM uzivatel WHERE nick=?", [nick]))
+            print(tabulka)
+        if tabulka and check_password_hash(tabulka[0][0], passwd):
+            flash("Ano!")
+        else:
+            flash("Ne!")
+    return redirect(url_for("index"))
+
+
+@app.route("/register/")
+def register():
+    return render_template("register.html.j2", slova=slova)
+
+
+@app.route("/register/", methods=["POST"])
+def register_post():
+    nick = request.form.get("nick")
+    passwd1 = request.form.get("passwd1")
+    passwd2 = request.form.get("passwd2")
+    if nick and passwd1 and passwd2 == passwd1:
+        hashpasswd = generate_password_hash('passwd1')
+        with sqlite3.connect(dbfile) as conn:
+            try:
+                conn.execute("INSERT INTO uzivatel (nick,passwd) VALUES (?,?)", [nick, hashpasswd])
+                flash("Uživatel vytvořen!")
+            except sqlite3.IntegrityError:
+                flash("Uživatel již existuje!")
+    else:
+        flash("Chyba: je nutné zadat správně veškeré údaje!")
+        return redirect(url_for("register"))
+    return redirect(url_for("index"))
 
 
 @app.route("/text/")
